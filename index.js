095147a380b3a00bc256508ef755ef4bc6736b81
lucky <takuto.yoshikai@gmail.com>
2021-05-03 15:59:34 +0900

    init

---
+++ index.js
@@ -0,0 +1,30 @@
+const { execSync } = require("child_process");
+
+function makeRecord(record) {
+  return {
+    command: record[0],
+    pid: parseInt(record[1]),
+    user: record[2],
+    fd: record[3],
+    type: record[4],
+    device: record[5],
+    size: record[6],
+    node: parseInt(record[7]),
+    name: record.slice(8).join(" "),
+  }
+}
+
+function lsof(params) {
+  const stdout = execSync("lsof " + params).toString();
+  const lines = stdout.split("\n");
+  lines.splice(0, 1);
+  lines.splice(lines.length - 1, 1);
+  const records = lines.map(line => {
+    return makeRecord(line.split(" ").filter(value => {
+      return value !== "";
+    }));
+  });
+  console.log(records);
+}
+
+module.exports = lsof;
